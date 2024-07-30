6da019c0a2dfaf105b4e34d0b627985eba15fcc9
hibbans <mhibbansyakir@gmail.com>
2024-07-30 12:29:45 +0700

    First commit of locust load test

---
+++ locustfile.py
@@ -0,0 +1,13 @@
+import time
+from locust import HttpUser, between, task
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def index(self):
+        self.client.get(url="/")
+        
+    @task
+    def about(self):
+        self.client.get(url="/articles/")
